23a238fa4199bb2f26734aaee94dbb383b855959
Julie Laffy <julielaffy@Julies-MacBook-Pro.local>
2019-06-17 17:40:13 +0300

    Adding functions to allow for (1) applying a function to all pairs of vectors in a list or betwee two lists (map_combinations()); (2) computing jaccard similarity; (3) ordering a list by significance and (4) filtering an ordered-list (where higher elements in the list are given priority) by jaccard similarity.

--- R/order_by_significance.R
+++ R/order_by_significance.R
@@ -3,7 +3,7 @@
 #' @description Reorders a list of named numeric vectors according to two parameters for significance, sig1 and sig2. sig1 is the number of elements in each vector. sig2 is the number of elements in each vector after filtering the list such that each element only appears once in the vector in which it's value was highest.
 #' @param L List of named numeric vectors (e.g. Gene names and FC values for different clusters)
 #' @param method character string indicating which ordering method should be used, one of 'combined', 'sequential', 'sig1' or 'sig2'. 'combined' orders by the mean of sig1 ranks and sig2 ranks. 'sequential' orders by sig1 and sig2 sequentially, ordering by the second only where there are ties in the first. 'sig1' orders by the number of elements in each vector. 'sig2' orders by the number of elements in each vector after filtering the list such that each element only appears once in the vector in which it's value is highest. Default: 'combined'
-#' @param character string specifying how ties are treated, see base::rank for more details.
+#' @param ties.method character string specifying how ties are treated, see base::rank for more details.
 #' @param first character string indicating whether ordering should be first by sig1 and then by sig2 (default) or vice versa. One of 'sig1' or 'sig2'. Only relevant if method is 'sequential'. Default: 'sig1'
 #' @param decreasing boolean value indicating whether the list should be reordered by decreasing or increasing significance. If TRUE, the most significant vector will appear first. Default: TRUE
 #' @param return.index boolean value indicating whether ordering index vector should be returned instead of sorted list. Default: FALSE
@@ -20,6 +20,7 @@ order_by_significance = function(L,
                                  method = c("combined", "sequential", "sig1", "sig2"),
                                  ties.method = c("average", "first", "last", "random", "max", "min"),
                                  first = c("sig1", "sig2"),
+                                 highest = TRUE,
                                  decreasing = TRUE,
                                  return.index = FALSE) {
      
@@ -28,7 +29,7 @@ order_by_significance = function(L,
     if (length(first) > 1) first = first[1]
 
     sig1 = lengths(L)
-    sig2 = lengths(uniquely(L))
+    sig2 = lengths(uniquely(L, highest = highest))
     rank1 = rank(sig1, ties.method = ties.method)
     rank2 = rank(sig2, ties.method = ties.method)
 
