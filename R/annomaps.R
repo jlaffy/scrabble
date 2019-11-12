2d2f0169a906e86c2a9e7256dcd25b88283b885b
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-11-12 18:55:55 +0200

    x.breaks renamed to breaks

--- R/annomaps.R
+++ R/annomaps.R
@@ -13,8 +13,8 @@
 #' @export 
 #' @importFrom ggplot2 waiver theme
 annomaps = function(..., titles = NULL, flip = F, ratio = 0.03, mar = 0.015) {
-    .annomap = function(X, pal, title = NULL, x.breaks = NULL) {
-        annomap(X, title = title, flip = flip, ratio = ratio, x.breaks = x.breaks, pal = pal)
+    .annomap = function(X, pal, title = NULL, breaks = NULL) {
+        annomap(X, title = title, flip = flip, ratio = ratio, breaks = breaks, pal = pal)
     }
     dots = list(...)
     lev = levels(dots[[1]])
@@ -23,7 +23,7 @@ annomaps = function(..., titles = NULL, flip = F, ratio = 0.03, mar = 0.015) {
     len = length(dots) - 1
     if (is.null(titles)) titles = rep('', len+1)
     maps = sapply(1:len, function(i) .annomap(X = dots[[i]], pal = i, title = titles[[i]]), simplify = F)
-    lastmap = list(.annomap(dots[[len+1]], title = titles[[len+1]], pal = len+1, x.breaks = ggplot2::waiver()))
+    lastmap = list(.annomap(dots[[len+1]], title = titles[[len+1]], pal = len+1, breaks = ggplot2::waiver()))
     maps = c(maps, lastmap)
     maps = sapply(maps, function(p) p + ggplot2::theme(plot.margin = margin(mar,mar,mar,mar,"cm")), simplify = F)
     if (flip) by = 'y'
