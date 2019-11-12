eb85063e71e9cfb3d264d5e26ffa20794c53e771
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-11-13 00:56:05 +0200

    minor

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,60 @@
+#' @title <dim> for many matrices
+#' @description Returns the result of dim for every matrix in a list
+#' @param m a list of matrices (or a single matrix)
+#' @return dim for each matrix provided.
+#' @rdname dims
+#' @export 
+dims <- function(mats) {
+    # if mats is a single matrix:
+    if (!is.null(dim(mats))) {
+        return(dim(mats))
+    }
+    # if mats is a list of matrices:
+    sapply(mats, dim, simplify = T)
+}
+
+
+#' @title <ncol> for many matrices
+#' @description Returns the result of ncol for every matrix in a list
+#' @param m a list of matrices (or a single matrix)
+#' @return ncol for each matrix provided.
+#' @rdname ncols
+#' @export 
+ncols <- function(mats) {
+    # if mats is a single matrix:
+    if (!is.null(dim(mats))) {
+        return(ncol(mats))
+    }
+    # if mats is a list of matrices:
+    sapply(mats, ncol, simplify = T)
+}
+
+
+#' @title <nrow> for many matrices
+#' @description Returns the result of nrow for every matrix in a list
+#' @param m a list of matrices (or a single matrix)
+#' @return nrow for each matrix provided.
+#' @rdname nrows
+#' @export 
+nrows <- function(mats) {
+    # if mats is a single matrix:
+    if (!is.null(dim(mats))) {
+        return(nrow(mats))
+    }
+    # if mats is a list of matrices:
+    sapply(mats, nrow, simplify = T)
+}
+
+
+#' @title Range of the rowMeans of a matrix
+#' @description Returns the range over the rowMeans of a matrix
+#' @param mat a matrix
+#' @return the range of the rowMeans of the matrix provided.
+#' @rdname range_rowMeans
+#' @export 
+range_rowMeans <- function(mat) {
+    range(rowMeans(mat))
+}
 
 is.corr.matrix = function(m) {
     m = as.matrix(m)
